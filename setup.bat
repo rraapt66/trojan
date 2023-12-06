8ec8795d7dac0474ae9835f35230dc191b30b2de
rraapt66 <146457670+rraapt66@users.noreply.github.com>
2023-12-06 20:38:04 +0700

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -4,4 +4,4 @@ python -m pip install -r requirements.txt
 cls
 echo enter!!!
 pause
-start https://www.dropbox.com/scl/fo/7019r1ngjdrb8n6apdis2/h?rlkey=jhg1vovxa8tvcklxpo609f7gz&dl=0
+start https://www.dropbox.com/home/Trojan
